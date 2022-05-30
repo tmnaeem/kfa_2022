4559d638864e7bd5b94c6e528d7b446ff431c3ed
tmnaeem <tnaeemtrashid@email.com>
2022-05-31 07:13:57 +0800

    add construction animation temporarily

--- kfa_2022/landing_page/urls.py
+++ kfa_2022/landing_page/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.user_landing_page, name='user_landing_page'),
+    path('', views.user_landing_page, name='user_landing_page'),
+    path('tournament_report/', views.tournament_report, name='tournament_report'),
+    path('about_us/', views.about_us, name='about_us'),
 ]
--- kfa_2022/landing_page/views.py
+++ kfa_2022/landing_page/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
 from django.core import serializers
 from django.contrib.auth import authenticate, login
+from admin_dashboard.models import *
 import json
 
 def user_landing_page(request):
+    tournaments_list = AvailableTournaments.objects.all().values('TOURNAMENT_TITLE')
+    tournaments_list = [ tournaments['TOURNAMENT_TITLE'] for tournaments in tournaments_list ]
     context = {
         'title' : 'Widad K-League',
         'year' : 2022,
         'location' : 'home',
+        'tournaments_list' : tournaments_list
     }
     return render(request, 'landing_page/landing_page.html', context)
+
+def tournament_report(request):
+    tournaments_list = AvailableTournaments.objects.all().values('TOURNAMENT_TITLE')
+    tournaments_list = [ tournaments['TOURNAMENT_TITLE'] for tournaments in tournaments_list ]
+    context = {
+        'title' : 'Widad K-League',
+        'year' : 2022,
+        'location' : 'home_tournament_report',
+        'tournaments_list' : tournaments_list
+    }
+    return render(request, 'landing_page/tournament_result.html', context)
+
+def about_us(request):
+    tournaments_list = AvailableTournaments.objects.all().values('TOURNAMENT_TITLE')
+    tournaments_list = [ tournaments['TOURNAMENT_TITLE'] for tournaments in tournaments_list ]
+    context = {
+        'title' : 'Widad K-League',
+        'year' : 2022,
+        'location' : 'home_about_us',
+        'tournaments_list' : tournaments_list
+    }
+    return render(request, 'landing_page/about_us.html', context)
